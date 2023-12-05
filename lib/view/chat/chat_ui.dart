8eb79d2e770966b054dbe4f8be1b31b6eb5fb701
codingking02 <zeyadismailo2002@gmail.com>
2023-12-05 21:08:05 +0200

    m

--- lib/view/chat/chat_ui.dart
+++ lib/view/chat/chat_ui.dart
@@ -9,23 +9,44 @@ class ChatUi extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          InkWell(
-            onTap: () {
-              showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return SizedBox(
-                    height: 375,
-                    child: getSvgPicture(
-                      'assets/menu.svg',
-                    ),
-                  );
-                },
-              );
-            },
-            child: getSvgPicture(
-              'assets/chat.svg',
+          gettopbarimage(),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: 24,
             ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        getSvgPicture(
+                          "assets/arrow-left.svg",
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Image.asset(
+                          "assets/twitterlogochat.png",
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          'Twitter',
+                          style: TextStyle(
+                            color: Color(0xFF111827),
+                            fontSize: 16,
+                            fontFamily: 'SF Pro Display',
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.16,
+                          ),
+                        ),
+                      ],
           ),
           Positioned(
             top: 55,
@@ -33,7 +54,12 @@ class ChatUi extends StatelessWidget {
             child: Image.asset(
               'assets/twitterlogo.png',
             ),
-          ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
